8870942cac45f8576e0c8d77d1f11c1c94702e5a
27082007vitor <161051919+27082007vitor@users.noreply.github.com>
2025-05-12 12:29:29 -0300

    aa

--- supabaseClient.js
+++ supabaseClient.js
@@ -4,4 +4,5 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 // Cria o client com a URL e a chave fornecidas
 export const supabase = createClient(
   'https://pbfmwplilmitvcatshhc.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBiZm13cGxpbG1pdHZjYXRzaGhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY1MzU0NjYsImV4cCI6MjA2MjExMTQ2Nn0.86d8etT6JnHp5A_9IlZbzbTWp4BTAVg4SYW3pRRlRw4')
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBiZm13cGxpbG1pdHZjYXRzaGhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY1MzU0NjYsImV4cCI6MjA2MjExMTQ2Nn0.86d8etT6JnHp5A_9IlZbzbTWp4BTAVg4SYW3pRRlRw4'
+)
